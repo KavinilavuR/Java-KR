a0457ae44fa2b84ca58481ca19401a81c80a6cc7
KavinilavuR <kavinilavu.r2024cce@sece.ac.in>
2025-07-09 00:04:10 +0530

    Create Love.java

---
+++ Love.java
@@ -0,0 +1,9 @@
+//Question:Combine and print the two statements "I Love Comics" and "they are sometimes boring" in the same line ,combine using any word of your choice
+public class Love {
+    public static void main(String[] args) {
+        String bore="they are sometimes boring";
+        System.out.print("I Love Comics ");
+        System.out.println("and"+bore);
+    }
+}
+//output:I Love Comics andthey are sometimes boring
