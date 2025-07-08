d9dc4e705e83de381638496894622a1b19726716
KavinilavuR <kavinilavu.r2024cce@sece.ac.in>
2025-07-08 23:54:22 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,14 @@
+//Question:to write a java program to print "hi" 
+//programming lang:java
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hi");
+    }
+}
+
+//java is case sensitive
+//if "Main" or the class Name is made with a case error/capitalization error like "main"
+//the case error of "Main" is "class main is public , should be declared in a file named main.java"
+//file name should be same as of the class Name(here Main)
+//println-represents print line(print whatever content inside ""(double quotes) and then move to next line(like adding a "\n" in C,C++)
+//print-just print(just print whatever is inside ""(double quotes/variables) and stay in the same line dont move to next line
