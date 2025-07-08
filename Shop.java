cdb3fc3318e56ab2eeba31dbd78b75120fb70d46
KavinilavuR <kavinilavu.r2024cce@sece.ac.in>
2025-07-08 12:05:42 +0530

    Create Shop.java

---
+++ Shop.java
@@ -0,0 +1,14 @@
+//scenario:weighing tomato of 2Kgs 
+//lang:java
+public class Shop
+{
+  public static void main(String[] args)
+  {
+    int tomato=2;
+    int weighing_M=0;
+    weighing_M=tomato;
+    System.out.print("weight:"+weighing_M);
+    
+   }
+}
+//commenting in java is the same as commenting in C
