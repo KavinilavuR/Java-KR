99830286f78d4b853d4bfcfd80dfc0a29e24dd50
KavinilavuR <kavinilavu.r2024cce@sece.ac.in>
2025-07-09 09:18:23 +0530

    Create concatenate.java

---
+++ concatenate.java
@@ -0,0 +1,13 @@
+//question:write a java program to declare and initialize a string variable and concatenate with some other string using '+' , the concatenation operator
+//programming lang:java
+public class concatenate
+{
+    public static void main(String[] args)
+    {
+        String name;
+        name="Kavin";
+        System.out.print("My Nick Name : "+name);
+    }
+}
+
+//output:My Nick Name : Kavin
