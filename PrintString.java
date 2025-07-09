6ce1a6fbccfe9e45b754d9afeae559541b04b036
KavinilavuR <kavinilavu.r2024cce@sece.ac.in>
2025-07-09 09:31:53 +0530

    Create PrintString.java

---
+++ PrintString.java
@@ -0,0 +1,13 @@
+//question : write a java program to print your name,street name,city name,country name
+public class PrintString
+{
+   public static void main(String[] args)
+  {
+     String E_name = "Kavin";
+     String S_name = "abc123 street";
+     String C_name = "pqrs city";
+     String CC_name = "xyz country";
+     System.out.print(E_name +" resides in " + S_name +" , "+ C_name +" , "+ CC_name);
+  }
+}
+//output:Kavin resides in abc123 street , pqrs city , xyz country
