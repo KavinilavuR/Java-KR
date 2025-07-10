5ad363f68c166267ee8b75d4a2a1d2361b316213
KavinilavuR <kavinilavu.r2024cce@sece.ac.in>
2025-07-10 15:12:17 +0530

    Update cubeSquare.java

--- cubeSquare.java
+++ cubeSquare.java
@@ -4,7 +4,12 @@ import java.util.Scanner;
 class methods
 {
 	private int value;
-	public void setter(int num)
+	/*
+	 * if the member variable(value) of the class "methods" would have not been 
+	 * under a private access specifier it can be accessed or initialized inside the main class itself
+	 * but as it is under a private access specifier there is a need of getter and a setter
+	 */
+	public void setter(int num) 
 	{
 		this.value=num;
 	}
@@ -17,6 +22,8 @@ class methods
 		return value*value;  
 	}
 }
+
+//not allowed to initialize any value inside a class
 public class cubeSquare {
 
 	public static void main(String[] args) {
@@ -24,6 +31,7 @@ public class cubeSquare {
 		int num;
 		Scanner sc = new Scanner(System.in);
 		methods obj = new methods();
+		System.out.print("Enter a Number:");
 		num = sc.nextInt();
 		obj.setter(num);
 		System.out.println("Cube of 3 :"+obj.Cube());
@@ -34,7 +42,7 @@ public class cubeSquare {
 }
 /*
 output:
-89
+Enter a Number:89
 Cube of 3 :704969
 Square of 3 :7921
 */
